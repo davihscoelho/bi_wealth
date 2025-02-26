bdbf81999235748409d9ec4acc5f2d84127a8eaf
Davi Henrique <davihscoelho@gmail.com>
2025-02-26 18:51:00 -0300

    begginng build xp ingestion pipeline full_load and cdc

--- src/ingestion_xp.py
+++ src/ingestion_xp.py
@@ -8,4 +8,101 @@ from pandas.tseries.offsets import BDay
 from dotenv import load_dotenv
 load_dotenv()
 
-# Get 
+# Step by step
+# 1. Autenticar
+# 2. Get portfolios
+# 3. Get portfolio AUM
+# 4. Get data from endpoints
+# 5. Save data to Datalake
+
+def autenticar():
+    
+    client_id = os.getenv("CLIENT_ID_XP")
+    client_secret = os.getenv("CLIENT_SECRETS_XP")
+    header = {"Content-Type":"application/x-www-form-urlencoded"}
+    data = {
+            'grant_type': "client_credentials",
+            "scope": 'api://xpcorretora.onmicrosoft.com/api-ws-assets-query-external-prd/.default',
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+    url = 'https://login.microsoftonline.com/cf56e405-d2b0-4266-b210-aa04636b6161/oauth2/v2.0/token'
+    response = requests.post(url, headers=header, data=data)
+    if response.status_code == 200:
+        print("✅ Request bem-sucedido!")
+        print(f"Token Gerado: Status Code {response.status_code}")
+        authorization =  response.json()["token_type"] + " " + response.json()["access_token"]
+        return authorization
+    else:
+        print("❌ Request mal-sucedido!")
+def get_portfolios():
+
+    authorization = autenticar()
+    header = {
+        "Authorization": authorization,
+        "Ocp-Apim-Subscription-Key": 'ada131f3cf3a41a2a0d8ce0048b43ad9'
+
+    }
+    url_base = "https://openapi.xpi.com.br/wealthservices-contracts/external"
+    url = f"{url_base}/api/v1/customers"
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        print("✅ Request bem-sucedido!")
+        print(f"Portfolios: Status Code {response.status_code}")
+        return response.json()
+
+def get_evolucao_aum(base_url, authorization, portfolio_id, params=None):
+    headers = {"Authorization": authorization}
+    response = requests.get(
+            url = f"{base_url}/v1/wealth-evolution/customer/{portfolio_id}",
+            params=params,
+            headers=headers
+        )
+    
+    if response.status_code == 200:
+        print("✅ Request bem-sucedido!")
+        resposta = response.json()
+        # Proceed with processing the data if it contains values
+        for i in resposta["timeseries"]:
+            i["portfolio_id"] = portfolio_id
+        return resposta
+    else:
+        print(f"❌ Nenhum dado encontrado para {portfolio_id} no periodo {params}!")
+        return 
+
+
+######################### TESTING #######################
+
+authorization = autenticar()    
+portfolios_ids = get_portfolios()
+portfolios_ids
+portfolio_id = portfolios_ids[-6]["customerCode"]
+#portfolio_id = "3134083"
+base_url = "https://openapi.xpi.com.br/ws-external-reports/api"
+params = {
+    "startDate": "2025-01-01",
+    "endDate": "2025-01-28"
+}
+response = get_evolucao_aum(base_url, authorization, portfolio_id, params=params)
+
+from utils import generate_date_dict
+dates = generate_date_dict(2024, 1, 2025, 1)
+dates
+
+
+
+
+
+response
+#response.keys()
+response.json()
+data = response["profit"]
+data
+
+data = pd.DataFrame(data)
+data
+
+
+#portfolio_id
+#response = autenticar()
+#response
---
+++ src/utils.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+import pandas as pd
+
+def generate_date_dict(start_year, start_month, end_year, end_month):
+    date_dict = {}
+    current_date = datetime(start_year, start_month, 1)
+
+    while current_date <= datetime(end_year, end_month, 1):
+        key = current_date.strftime("%Y-%m")  # Format as "YEAR-Month"
+
+        date_dict[key] = {
+            "startDate": current_date.strftime("%Y-%m-%d"),
+            "endDate": (current_date.replace(day=28)).strftime("%Y-%m-%d")
+        }
+
+        # Move to the next month
+        next_month = current_date.month + 1
+        next_year = current_date.year + (1 if next_month > 12 else 0)
+        current_date = datetime(next_year, next_month if next_month <= 12 else 1, 1)
+
+    return date_dict
